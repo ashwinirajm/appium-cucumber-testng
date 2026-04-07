dcc22a47b478095c7cfdaa2708e30608d618b1dc
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-04-07 19:11:22 +0530

    Enhance TestRunner with plugins and parallel execution
    Updated Cucumber options to include additional plugins and enabled parallel execution of scenarios.

--- src/test/java/com.company.automation/runners/TestRunner.java
+++ src/test/java/com.company.automation/runners/TestRunner.java
@@ -1,7 +1,20 @@
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.company.automation.stepdefinitions",
-        plugin = {"pretty"}
+        glue = {"steps"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
+        },
+        monochrome = true,
+        publish = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    // 🔥 Enables parallel execution of scenarios
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
